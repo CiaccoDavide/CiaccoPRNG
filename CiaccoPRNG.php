56e64c242ded07f7cade99dbd090769868451bce
Ciacco Davide <CiaccoDavide@users.noreply.github.com>
2015-11-04 23:53:57 +0100

    Update CiaccoPRNG.php

--- CiaccoPRNG.php
+++ CiaccoPRNG.php
@@ -1,5 +1,7 @@
 <?php
+
   namespace ciacco_PRNG;
+  
   class CiaccoPRNG {
   
     private static $tree = 0;
@@ -10,6 +12,7 @@
       self::getRand();
     }
     
+    // get a random int between $min and $max [both included]
     public static function getRand($min = 0, $max = 9999999) {
       self::$tree = (self::$tree * 123) % 69522569;
       return self::$tree % ($max - $min + 1) + $min;
